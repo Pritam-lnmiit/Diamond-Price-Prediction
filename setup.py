539d8ebab96f35ec2f0fe816e5b4b4a93259a782
Pritam Das <159282284+Pritam-lnmiit@users.noreply.github.com>
2025-04-29 17:32:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,15 +11,15 @@ def get_requirements(file_path:str)->List[str]:
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
+
         return requirements
 
 
-
 setup(
-    name='DiamondPriceprediction',
+    name='DiamondPricePrediction',
     version='0.0.1',
-    author='Pwskills',
-    author_email="example@gmail.com",
+    author='Pritam',
+    author_email='pritamlnmiit.work@gmail.com',
     install_requires=get_requirements('requirements.txt'),
     packages=find_packages()
 
